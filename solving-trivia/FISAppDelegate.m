6286cf35e4cf8b82428b2867b8a35342e0a24fa8
Rama Milaneh <ramamilaneh@gmail.com>
2017-02-12 16:16:19 -0500

    Done

--- solving-trivia/FISAppDelegate.m
+++ solving-trivia/FISAppDelegate.m
@@ -6,13 +6,87 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSString *result = [self solveTrivia];
+    NSLog(@"%@",result);
     return YES;
 }
 
-/*
+-(NSString *)solveTrivia {
+    NSDictionary *stateCapitals= @{@"Alabama":@"Montgomery",
+                                   @"Alaska":@"Juneau",
+                                   @"Arizona":@"Phoenix",
+                                   @"Arkansas":@"Little Rock",
+                                   @"California":@"Sacramento",
+                                   @"Colorado":@"Denver",
+                                   @"Connecticut":@"Hartford",
+                                   @"Delaware":@"Dover",
+                                   @"Florida":@"Tallahassee",
+                                   @"Georgia":@"Atlanta",
+                                   @"Hawaii":@"Honolulu",
+                                   @"Idaho":@"Boise",
+                                   @"Illinois":@"Springfield",
+                                   @"Indiana":@"Indianapolis",
+                                   @"Iowa":@"Des Moines",
+                                   @"Kansas":@"Topeka",
+                                   @"Kentucky":@"Frankfort",
+                                   @"Lousiana":@"Baton Rouge",
+                                   @"Maine":@"Augusta",
+                                   @"Maryland":@"Annapolis",
+                                   @"Massassachusetts":@"Boston",
+                                   @"Michigan":@"Lansing",
+                                   @"Minnesota":@"Saint Paul",
+                                   @"Mississippi":@"Jackson",
+                                   @"Missouri":@"Jefferson City",
+                                   @"Montana":@"Helena",
+                                   @"Nebraska":@"Lincoln",
+                                   @"Nevada":@"Carson City",
+                                   @"New Hampshire":@"Concord",
+                                   @"New Jersey":@"Trenton",
+                                   @"New Mexico":@"Santa Fe",
+                                   @"New York":@"Albany",
+                                   @"North Carolina":@"Raleigh",
+                                   @"North Dakota":@"Bismarck",
+                                   @"Ohio":@"Columbus",
+                                   @"Oklahoma":@"Oklahoma City",
+                                   @"Oregon":@"Salem",
+                                   @"Pennsylvania":@"Harrisburg",
+                                   @"Rhode Island":@"Providence",
+                                   @"South Carolina":@"Columbia",
+                                   @"South Dakota":@"Pierre",
+                                   @"Tennessee":@"Nashville",
+                                   @"Texas":@"Austin",
+                                   @"Utah":@"Salt Lake City",
+                                   @"Vermont":@"Montpelier",
+                                   @"Virgina":@"Richmond",
+                                   @"Washington":@"Olympia",
+                                   @"West Virginia":@"Charleston",
+                                   @"Wisconsin":@"Madison",
+                                   @"Wyoming":@"Cheyenne"};
+    BOOL isTrivia = NO;
+    
+    for(NSString *key in stateCapitals) {
+        
+        NSString *newKey = [NSString stringWithString:key];
+        NSString *newvalue = [NSString stringWithString:stateCapitals[key]];
+        newKey = [[key stringByReplacingOccurrencesOfString:@" " withString:@""] lowercaseString];
+        newvalue = [[stateCapitals[key] stringByReplacingOccurrencesOfString:@" " withString:@""] lowercaseString];
+        NSMutableSet *keyCharacters = [NSMutableSet set];
+        NSMutableSet *valueCharacters = [NSMutableSet set];
 
-Write your method here
-
-*/
+        for(NSUInteger i = 0; i<[newKey length]; i ++){
+            [keyCharacters addObject:[newKey substringWithRange:NSMakeRange(i, 1)]];
+        }
+        for(NSUInteger i = 0; i<[newvalue length]; i ++){
+            [valueCharacters addObject:[newvalue substringWithRange:NSMakeRange(i, 1)]];
+        }
+       
+        if ([keyCharacters intersectsSet:valueCharacters] == NO) {
+            isTrivia = YES;
+            return [NSString stringWithString:key];
+        }
+        
+    }
+    return nil;
+    }
 
 @end
